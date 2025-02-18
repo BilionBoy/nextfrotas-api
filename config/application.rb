50fcbbf502de5ae3b8876389a1ea317757f61823
Dhiogo <dhiogoandre26@gmail.com>
2025-02-18 09:24:40 -0400

    wip: generator API

--- config/application.rb
+++ config/application.rb
@@ -1,28 +1,26 @@
 require_relative "boot"
-
 require "rails/all"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module ApiMetricas
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
+
+    # Rails version.
     config.load_defaults 7.2
 
     # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.generators do |g|
+      g.scaffold_controller "scaffold/api/controller"            # Aqui informamos o caminho do gerador
+    end
     config.autoload_lib(ignore: %w[assets tasks])
     config.autoload_paths += %W[#{config.root}/app/queries]
 
+    config.time_zone = "La Paz"                                   # Configuração do Time Zone
+    config.i18n.default_locale = :'pt-BR'                         # Configuração do idioma padrão
 
-    config.time_zone = "La Paz" # Configuração do Time Zone
-
-    # config.time_zone = "Central Time (US & Canada)"
-    config.i18n.default_locale = :'pt-BR' # Configuração do idioma padrão
-
-
-    # Configuração para uma aplicação apenas API
+    config.autoload_paths += Dir[Rails.root.join("lib", "**/")]   # Certificando que subpastas sejam carregadas
+    config.eager_load_paths += Dir[Rails.root.join("lib", "**/")] # Configuração para uma aplicação apenas API
     config.api_only = true
   end
 end
