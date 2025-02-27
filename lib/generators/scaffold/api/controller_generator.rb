ce6d9a9489d605bb0f2e05db2c1d837db060023c
Dhiogoandre <andreaguiar@seduc.ro.gov.br>
2025-02-27 08:07:21 -0400

    refactor: custom generator controller

--- lib/generators/scaffold/api/controller_generator.rb
+++ lib/generators/scaffold/api/controller_generator.rb
@@ -1,35 +1,21 @@
 # frozen_string_literal: true
+
 require "rails/generators"
 
 module Scaffold
   module Api
+    # Gerador de Controllers para API
     class ControllerGenerator < Rails::Generators::NamedBase
       source_root File.expand_path("templates", __dir__)
 
-      argument :attributes, type: :array, default: []
-
-      # Gera o arquivo do controller com base no nome e atributos fornecidos
       def create_controller_file
-        namespace = "V1"
-        plural_file_name = file_name.pluralize
-        file_path = controller_file_path(namespace, plural_file_name)
-
-        @controller_class_name = plural_file_name.camelize
-        @assign_attributes = formatted_attributes
-
-        template "controller.rb.tt", file_path
+        template "controller.rb.tt", controller_file_path
       end
 
       private
 
-      # Retorna o caminho correto para salvar o arquivo do controller
-      def controller_file_path(namespace, plural_file_name)
-        File.join("app/controllers", namespace.underscore, "#{plural_file_name}_controller.rb")
-      end
-
-      # Formata os atributos fornecidos, organizando-os como nome e tipo
-      def formatted_attributes
-        attributes.map { |attr| { name: attr.name, type: attr.type } }
+      def controller_file_path
+        File.join("app/controllers/v1", "#{file_name.pluralize}_controller.rb")
       end
     end
   end
