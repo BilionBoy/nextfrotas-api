8690fe79c6579e9d43b16b0c379a3fc4eba12e23
Dhiogoandre <andreaguiar@seduc.ro.gov.br>
2025-03-25 10:22:16 -0400

    refactor: serializer vw-salas_aulas

--- app/controllers/api/v1/vw_salas_aulas_controller.rb
+++ app/controllers/api/v1/vw_salas_aulas_controller.rb
@@ -5,7 +5,8 @@ module Api::V1
 
     def index
       response = paginate(VwSalaAula.ransack(params[:q]).result.order(:id), params[:per_page])
-      render_success(data: response.slice(:pagy, :items).merge(items: response[:items].as_json(only: [ :escola, :cre, :capacidade, :id ])), message: "Vw sala aulas listados com sucesso")
+
+      render_success(data: response.slice(:pagy, :items).merge(items: response[:items].as_json(only: [ :escola, :cre, :capacidade, :tipo_ambiente_predial, :id ])), message: "Sucesso!")
     end
   end
 end
--- app/serializers/vw_sala_aula_serializer.rb
+++ app/serializers/vw_sala_aula_serializer.rb
@@ -1,4 +1,4 @@
 # app/serializers/vw_sala_aula_serializer.rb
 class VwSalaAulaSerializer < ActiveModel::Serializer
-  attributes :escola, :cre, :capacidade, :id
+  attributes :escola, :cre, :capacidade, :tipo_ambiente_predial, :id
 end
