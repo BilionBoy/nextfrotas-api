d3323390bc12b30d6df9d10ea1e3e369d7619f94
BilionBoy <dhiogoandre26@gmail.com>
2025-11-15 20:58:08 -0400

    corrigindo centros de custo

--- app/controllers/api/v1/g_centros_custos_controller.rb
+++ app/controllers/api/v1/g_centros_custos_controller.rb
@@ -4,20 +4,26 @@ module Api
   module V1
     class GCentrosCustosController < ApplicationController
       include JsonResponse
+      include PagyPagination
       before_action :require_gestor!
 
-      # GET /api/v1/centros_custos
+      # GET /api/v1/centros_custo
       def index
         centros = GCentroCusto
                     .where(a_unidade_id: current_user.a_unidade_id)
                     .includes(:g_tipo_centro_custo)
 
+        pagy_data = paginate(centros, params[:per_page])
+
         render_success(
           message: "Centros de custo listados com sucesso",
-          data: ActiveModelSerializers::SerializableResource.new(
-            centros,
-            each_serializer: GCentroCustoSerializer
-          )
+          data: {
+            pagy: pagy_data[:pagy],
+            items: ActiveModelSerializers::SerializableResource.new(
+              pagy_data[:items],
+              each_serializer: GCentroCustoSerializer
+            )
+          }
         )
       end
     end
--- app/models/g_centro_custo.rb
+++ app/models/g_centro_custo.rb
@@ -1,5 +1,6 @@
 class GCentroCusto < ApplicationRecord
   self.table_name = "g_centros_custos"
 
-  belongs_to :g_tipo_centro_custo
+  belongs_to :g_tipo_centro_custo, class_name: "GTipoCentroCusto", foreign_key: "g_tipo_centro_custo_id"
+  belongs_to :a_unidade,           class_name: "AUnidade",         foreign_key: "a_unidade_id"
 end
